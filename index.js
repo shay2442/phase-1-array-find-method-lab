ffebbe9a711fc121474464140216acc7becea9d9
Shay Wacker <shay2442@yahoo.com>
2021-08-18 12:32:30 -0500

    commit tree 2eaf82a340586ab191fb0d6f05e83236b61105b2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+  
+function superbowlWin(record) {  
+  
+    const yearCheifsWon = record.find( ( obj ) => obj.result === 'W')  
+     
+    if (yearCheifsWon) {
+        console.log("I am here", yearCheifsWon)
+    return yearCheifsWon.year
+    }
+    else {
+    return undefined
+    } 
+    };
+
+
+
+//const superbowlWin = record.find(  ({ result }) => result === 'W')
+
+//console.log("I am here: ", superbowlWin)
+//( ({ result }) => result === 'W')cs
